b4b1c065ab22512922994176899dfb822d192cdb
chris.bridges <christopher_bridges@icloud.com>
2018-02-18 19:25:08 -0500

    additional income forms generate on click

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
 const ALPHA_VANTAGE_API_KEY = 'YFIGM2L9A2765AB9';
-
 const ALPHA_VANTAGE_ENDPOINT = 'https://www.alphavantage.co/query';
 
+function addAdditionalIncomeInput () {
+  const incomeSelect = `<br><select>
+        <option value="0">Salary / Wages</option>
+        <option value="1">Additional Wages</option>
+        <option value="2">Pension</option>
+        <option value="3">Social Security</option>
+        <option value="4">Other</option>
+        <label for="income"></label>
+        <input type="number" min="0" step="100" name="income" id="income" placeholder="After-tax income">
+      </select>`;
+
+  $('.add-additional').on('click', function() {
+    $('#income-form').append(incomeSelect);
+  });
+}
+
+$(addAdditionalIncomeInput);
