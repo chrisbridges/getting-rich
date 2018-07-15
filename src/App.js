30e2740cdb4e165f351643cf991ec593a20d8e26
chris.bridges <christopher_bridges@icloud.com>
2018-07-15 16:42:32 -0400

    basic intro component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+// import Header from './components/Header';
+import Intro from './components/Intro';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      
+        <Intro />
       </div>
     );
   }
---
+++ src/components/Intro.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+ const Intro = () => {
+  return (
+    <div className="Intro">
+      <header>
+        <h1>Am I Getting Rich</h1>
+        <h2>(or nah)?</h2>
+      </header>
+      <p>We are a financial app that aims to provide a long-term perspective on your finances and allow you to see the impact they have on your wealth accumulation.</p>
+    </div>
+  );
+}
+
+export default Intro;
